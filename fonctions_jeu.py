59f1751ed7209867cfb37d19db4895d327dadd6c
leofaivre <leo.faivre@free.fr>
2025-04-01 15:23:34 +0200

    Delete fonctions_jeu.py
    inutile en fait

--- fonctions_jeu.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-def  test_successful(guess,co,cu):
-    print(cu)
-    print(guess)
-    result = cu.execute("SELECT * from Teachers WHERE nom ={}".format(guess))
-    return result
