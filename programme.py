4a0d3d4120e75d193f85092e89ac2f1835946b02
leofaivre <leo.faivre@free.fr>
2025-04-16 11:35:10 +0200

    ajout des professeurs et des modifications à l'algorithme pour plus de compréhension

--- programme.py
+++ programme.py
@@ -3,7 +3,9 @@ import sqlite3
 import del_database
 import random
 #lors du premier lancement, mettre en commentaire la ligne suivante
+
 del_database.delete_table()
+
 
 co = sqlite3.connect("TeacherdleDB.db") #recuperation du connecteur
 cu = co.cursor() #recuperation du curseur
@@ -11,7 +13,7 @@ cu = co.cursor() #recuperation du curseur
 create_database.initialisation_table(co,cu)# on initialise la table avec ces deux éléments
 
 #fin de la mise en place, on commence le jeu
-a = random.randint(1, 2)    #choix d un id de professeur aléatoire
+a = random.randint(1,26)    #choix d un id de professeur aléatoire
 objectif = cu.execute("SELECT nom from Teachers WHERE id={}".format(a)) 
 print(cu)
 nom_objectif = objectif.fetchall()   #on récupère le nom du professeur "cible" et on le montre pour l'instant
@@ -27,15 +29,20 @@ while win !=1 and nbr_essais != 0:
     print(PROF_RECHERCHE[0]) #nom du prof cible
     print(guess) #voila ce que l on guess
     if PROF_RECHERCHE[0] == guess: #on compare si c'est la même chose
-        print("win") 
-        break # fin de buzz si c est win
+        
+        for i in range (6):
+            print("vrai")
+        print("win")
+        # fin de buzz si c est win
+        
     else: 
         result_guess_not_fetched = cu.execute("SELECT * from Teachers WHERE nom ='{}' ".format(guess)) #les données de notre résultat
         result_guess = result_guess_not_fetched.fetchall()  # le tableau propre de nos données
         print(result_guess) #voici notre guess avec ces infos
-        for i in range (8):
+        for i in range (6):
             if (result_guess[0])[i] == (tableau_recherche[0])[i]: #algo de verification
                 print("vrai")
             else :
                 print("faux")
         print(" fin des infos, on peut re guess")
+        
